44ba62eb0bfcdeebf9df1dd50aae26392a5dfb18
Razvan Mutu <razvan.mutu10@gmail.com>
2022-11-19 03:36:53 +0100

    Implemented BG scrolling + ship movement

---
+++ SideScrollingSpaceShooter/Assets/Scripts/ScrollBG.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBG : MonoBehaviour
+{
+    [SerializeField] private float scrollSpeed = 0.1f;
+    private float xScroll;
+
+    [SerializeField] private MeshRenderer mRend;
+
+    private void Awake()
+    {
+        mRend = GetComponent<MeshRenderer>();    
+    }
+
+    private void Update()
+    {
+        Scroll();
+    }
+
+    private void Scroll()
+    {
+        xScroll = Time.time * scrollSpeed;
+        Vector2 offset = new Vector2(xScroll, 0f);
+
+        mRend.material.mainTextureOffset = offset;
+    }
+}
