04a250258989e888d0344a9ba6e1f7f389bcca32
Razvan Mutu <razvan.mutu10@gmail.com>
2022-11-21 16:28:45 +0100

    Implemented Main menu and pause screen

--- SideScrollingSpaceShooter/Assets/Scripts/Managers/GameController.cs
+++ SideScrollingSpaceShooter/Assets/Scripts/Managers/GameController.cs
@@ -6,12 +6,30 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
     public GameObject deathScreen;
+    public GameObject pauseScreen;
     private bool done = false;
+    private bool dead = false;
+    private bool paused = false;
+    public GameObject player;
 
     private void Update()
     {
+        if (deathScreen == null)
+            return;
+        if (pauseScreen == null)
+            return;
+        if (player == null)
+            return;
+
         if (PlayerController.instance.health <= 0.0f && !done)
             ActivateDeathScreen();
+
+        if (!dead)
+            if (Input.GetKeyDown(KeyCode.Escape))
+                if (!paused)
+                    ActivatePauseScreen(true, 0);
+                else
+                    ActivatePauseScreen(false, 1);
     }
 
     private void ActivateDeathScreen()
@@ -19,8 +37,20 @@ public class GameController : MonoBehaviour
         done = true;
 
         PlayerController.instance.gameObject.SetActive(false);
-        
+
+        pauseScreen.SetActive(false);
         deathScreen.SetActive(true);
+    }
+
+    private void ActivatePauseScreen(bool action, int val)
+    {
+        paused = action;
+
+        player.SetActive(!action);
+
+        Time.timeScale = val;
+
+        pauseScreen.SetActive(action);
     }
 
     public void Retry()
@@ -28,8 +58,21 @@ public class GameController : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
+
+    public void Resume()
+    {
+        ActivatePauseScreen(false, 1);
+    }
     public void Menu()
     {
-       
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+
     }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
 }
